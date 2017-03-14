78e2415e9dae50b29c0e4ec73a01a65b6a28eef9
Roman Roelofsen <romanroe@gmail.com>
2017-03-14 16:12:59 +0100

    Added ReactiveComponents

--- frontend/app/components/states.service.ts
+++ frontend/app/components/states.service.ts
@@ -1,21 +1,19 @@
-import {WorkPackageTimelineTableController} from './wp-table/timeline/wp-timeline-container.directive';
-import {whenDebugging} from '../helpers/debug_output';
-import {WorkPackageTable} from './wp-fast-table/wp-fast-table';
-import {
-  WorkPackageTableRow,
-  WPTableHierarchyState,
-  WPTableRowSelectionState
-} from './wp-fast-table/wp-table.interfaces';
-import {MultiState, initStates, State} from "../helpers/reactive-fassade";
+import {WorkPackageTimelineTableController} from "./wp-table/timeline/wp-timeline-container.directive";
+import {WorkPackageTable} from "./wp-fast-table/wp-fast-table";
+import {WPTableHierarchyState, WPTableRowSelectionState} from "./wp-fast-table/wp-table.interfaces";
+import {MultiState, State} from "../helpers/reactive-fassade";
 import {WorkPackageResource} from "./api/api-v3/hal-resources/work-package-resource.service";
 import {opServicesModule} from "../angular-modules";
-import {SchemaResource} from './api/api-v3/hal-resources/schema-resource.service';
-import {TypeResource} from './api/api-v3/hal-resources/type-resource.service';
-import {WorkPackageEditForm} from './wp-edit-form/work-package-edit-form';
-import {WorkPackageTableMetadata} from './wp-fast-table/wp-table-metadata';
-import {Subject} from 'rxjs';
+import {SchemaResource} from "./api/api-v3/hal-resources/schema-resource.service";
+import {TypeResource} from "./api/api-v3/hal-resources/type-resource.service";
+import {WorkPackageEditForm} from "./wp-edit-form/work-package-edit-form";
+import {WorkPackageTableMetadata} from "./wp-fast-table/wp-table-metadata";
+import {Subject} from "rxjs";
+import {Component} from "reactivecomponents";
+import {createNewContext} from "../../node_modules/reactivecomponents/dist/Context";
+import {whenDebugging} from "../helpers/debug_output";
 
-export class States {
+export class States extends Component {
 
   /* /api/v3/work_packages */
   workPackages = new MultiState<WorkPackageResource>();
@@ -38,15 +36,15 @@ export class States {
     // Table row selection state
     selection: new State<WPTableRowSelectionState>(),
     // Current state of collapsed groups (if any)
-    collapsedGroups: new State<{[identifier:string]: boolean}>(),
+    collapsedGroups: new State<{[identifier: string]: boolean}>(),
     // Hierarchies of table
     hierarchies: new State<WPTableHierarchyState>(),
     // State to be updated when the table is up to date
-    rendered:new State<WorkPackageTable>(),
+    rendered: new State<WorkPackageTable>(),
     // State to determine timeline visibility
     timelineVisible: new State<boolean>(),
     // Subject used to unregister all listeners of states above.
-    stopAllSubscriptions:new Subject()
+    stopAllSubscriptions: new Subject()
   };
 
   // Query states
@@ -61,14 +59,16 @@ export class States {
   // Open editing forms
   editing = new MultiState<WorkPackageEditForm>();
 
-  constructor() {
-    initStates(this, function (msg: any) {
-      whenDebugging(() => {
-        console.debug(msg);
-      });
-    });
-  }
-
 }
 
-opServicesModule.service('states', States);
+
+const ctx = createNewContext();
+const states = ctx.create(States);
+
+whenDebugging(() => {
+  states.loggingFn = (msg: string) => {
+    console.debug(msg);
+  }
+});
+
+opServicesModule.value('states', states);
